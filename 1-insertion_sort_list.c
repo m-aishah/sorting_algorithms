2176ccf0babf9ede1f5ceb08b38805697e2b5597
Aishah <maishah2540@gmail.com>
2023-08-26 06:42:25 -0700

    Task 2. Betty error fixed

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,7 +4,7 @@
  * swap_with_prev - Swap current node and previous nodes' positions.
  * @t: Pointer to the current node
  */
-void swap_with_prev(listint_t **t) 
+void swap_with_prev(listint_t **t)
 {
 	listint_t *c = *t;
 	listint_t *p = (*t)->prev;
@@ -22,7 +22,7 @@ void swap_with_prev(listint_t **t)
 }
 
 /**
- * insertion_sort_list - Sort a doubly linked list of integers 
+ * insertion_sort_list - Sort a doubly linked list of integers
  *		using the insertion sort.
  * @list: Pointer to the head of the linked list.
  *
@@ -34,17 +34,17 @@ void insertion_sort_list(listint_t **list)
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	
+
 	t = (*list)->next;
-	
-	while (t != NULL) 
+
+	while (t != NULL)
 	{
 		current = t;
-		
+
 		while (current->prev != NULL && current->n < current->prev->n)
 		{
 			swap_with_prev(&current);
-			
+
 			if (current->prev == NULL)
 				*list = current;
 			print_list(*list);
@@ -52,4 +52,3 @@ void insertion_sort_list(listint_t **list)
 		t = t->next;
 	}
 }
-
