bb5e6654f7713d5376d9f9799803db290a6c04be
Aishah <maishah2540@gmail.com>
2023-08-29 11:52:53 -0700

    Task 8.

---
+++ 104-heap_sort.c
@@ -0,0 +1,70 @@
+#include "sort.h"
+
+void build_heap(int *array, size_t size);
+void heapify(int *array2, size_t size, size_t leaf_index, size_t root_index);
+
+/**
+ * swap - Swap two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ */
+void swap(int *a, int *b)
+{
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
+
+/**
+ * heap_sort - Sort an array of integers using the heap sort algorithm.
+ *		The integers are sorted in ascending order
+ * @array: An array of integers to be sorted.
+ * @size: The number of integers in an array.
+ */
+void heap_sort(int *array, size_t size)
+{
+	int i;
+
+	if (array == NULL || size < 2)
+		return;
+
+	build_heap(array, size);
+
+
+	for (i = size - 1; i > 0; i--)
+	{
+		swap(array, array + i);
+		print_array(array, size);
+		heapify(array, size, i, 0);
+	}
+}
+
+void build_heap(int *array, size_t size)
+{
+	int i;
+
+	for (i = (size / 2) - 1; i >= 0; i--)
+		heapify(array, size, size, i);
+}
+
+void heapify(int *array, size_t size, size_t leaf_index, size_t root_index)
+{
+	size_t left, right, max;
+
+	left = (2 * root_index) + 1;
+	right = (2 * root_index) + 2;
+	max = root_index;
+
+	if (left < leaf_index && array[left] > array[max])
+		max = left;
+	
+	if ((right < leaf_index) && (array[right] > array[max]))
+		max = right;
+
+	if (max != root_index)
+	{
+		swap(array + root_index, array + max);
+		print_array(array, size);
+		heapify(array, size, leaf_index, max);
+	}
+}
