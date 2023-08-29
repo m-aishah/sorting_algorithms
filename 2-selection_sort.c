126b04ed7165a75384b0dd2cf03b9c009407e424
Ezejanu <Ezejanu_nno@ymail.com>
2023-08-28 21:12:12 -0700

    fixed gcc errors

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,17 +2,17 @@
 
 void selection_sort(int *array, size_t size)
 {
-	int i = 0;
-	int j = 0;
-	int min_index;
+	size_t i = 0;
+	size_t j = 0;
+	size_t min_index, temp;
 
 	if(size < 2)
 		return;
 
-	for(; i < size - 1, i++)
+	for(; i < size - 1; i++)
 	{
 		min_index = i;
-		for (int j = i +1; j < size; j++)
+		for(j = i + 1; j < size; j++)
 		{
 			if(array[j] < array[min_index])
 				min_index = j;
