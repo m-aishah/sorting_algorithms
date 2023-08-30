2976be97204fef137667993384ed461c7cbc4d5c
Aishah <maishah2540@gmail.com>
2023-08-29 20:20:32 -0700

    Edited 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,31 +1,61 @@
 #include "sort.h"
 
-void partition(int *array, size_t size, int *boundary_index)
+/**
+ * swap - Swaps the position of two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ */
+void swap(int *a, int *b)
 {
-	int i;
-	int pivot = array[size - 1];
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
 
-	*boundary_index = 0;
+int partition(int *array, size_t size, int l, int r)
+{
+	int i, h;
+	int *pivot = array + r;
 
-	for (i = 0; i < size - 1; i++)
+	for (h = i = l; i < r; i++)
 	{
-		if (array[i] < pivot)
+		if (array[i] < *pivot)
 		{
-			swap(&array[i], &array[boundary_index]);
-			(*boundary_index)++;
+			if (h < i)
+			{
+				swap(array + i, array + h);
+				print_array(array, size);
+			}
+			h++;
 		}
 	}
-	swap(&pivot, &array[*boundary_index]);
+
+	if (array[h] > *pivot)
+	{
+		swap(array + h, pivot);
+		print_array(array, size);
+	}
+
+	return (h);
+}
+
+void lomuto_sort(int *array, size_t size, int l, int r)
+{
+	int section;
+
+	if (r - l > 0)
+	{
+		section = partition(array, size, l, r);
+		lomuto_sort(array, size, l, section - 1);
+		lomuto_sort(array, size, section + 1, r);
+	}
 }
 
 void quick_sort(int *array, size_t size)
 {
-	int boundary_index;
-
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
 
-	partition(array, max_value - min_value + 1, &boundary_index);
 
-	quick_sort(array, boundary_index)
+	lomuto_sort(array, size, 0, size - 1);
 }
